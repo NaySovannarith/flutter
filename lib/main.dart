7784a5edfaee33be50be2d74564b847aeb69c72d
NaySovannarith <narith2004@gmail.com>
2025-04-02 18:24:44 +0700

    UI login

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,368 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyPortfolio());
+  runApp(const MainApp());
 }
 
-class MyPortfolio extends StatelessWidget {
-  const MyPortfolio({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: RegisterPage(),
       debugShowCheckedModeBanner: false,
-      home: PortfolioScreen(),
+      theme: ThemeData(fontFamily: 'Lato'),
     );
   }
 }
 
-class PortfolioScreen extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  bool termsAccepted = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 21, 158, 106),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20, 75, 20, 75),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CircleAvatar(
-              radius: 60,
-              backgroundImage: AssetImage('Profile-vannarith.jpg'),
+            Text(
+              'Register new\naccount',
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
             ),
             SizedBox(height: 20),
-            Text(
-              "Hi I'm ",
-              style: TextStyle(fontSize: 18, color: Colors.white),
-            ),
-            Text(
-              "Nay Sovannarith",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+            Image(image: AssetImage('assets/images/accent.png'), width: 100),
+            SizedBox(height: 50),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(color: Colors.grey[500]),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(36, 81, 80, 82),
               ),
             ),
-            Text(
-              "Developer",
-              style: TextStyle(fontSize: 22, color: Colors.white70),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Text(
-                "Collaborating with highly skilled individuals, our agency delivers top-quality services.",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, color: Colors.white70),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Action for Hire Me
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 160, 164, 175),
-                foregroundColor: const Color.fromARGB(255, 28, 31, 173),
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+            SizedBox(height: 30),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: TextStyle(color: Colors.grey[500]),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(36, 81, 80, 82),
+                suffixIcon: Icon(
+                  Icons.visibility_outlined,
+                  color: Colors.grey[500],
                 ),
               ),
-              child: Text("Hire Me!"),
             ),
-            SizedBox(height: 10),
-            OutlinedButton(
-              onPressed: () {
-                // Action for Download CV
-              },
-              style: OutlinedButton.styleFrom(
-                side: BorderSide(color: Colors.white),
-                foregroundColor: Colors.white,
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+            SizedBox(height: 30),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password Confirmation',
+                hintStyle: TextStyle(color: Colors.grey[500]),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(36, 81, 80, 82),
+                suffixIcon: Icon(
+                  Icons.visibility_outlined,
+                  color: Colors.grey[500],
                 ),
               ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text("Download CV"),
-                  SizedBox(width: 5),
-                  Icon(Icons.download, color: Colors.white),
-                ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Checkbox(
+                  side: BorderSide(color: Colors.grey),
+                  value: termsAccepted,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      termsAccepted = value!;
+                    });
+                  },
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: "By creating an account, you agree to our\n",
+                      ),
+                      TextSpan(
+                        text: 'Terms & Conditions',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const DashboardPage(),
+                    ),
+                  );
+                },
+                child: Text('Register', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            SizedBox(height: 90),
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(fontSize: 14, color: Colors.black),
+                  children: <TextSpan>[
+                    TextSpan(text: "Already have an account?"),
+                    TextSpan(
+                      text: ' Log in',
+                      style: TextStyle(color: Colors.blue),
+                      recognizer:
+                          TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.pop(context);
+                            },
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool rememberMe = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(20, 75, 20, 75),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Login to your\naccount',
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            Image(image: AssetImage('assets/images/accent.png'), width: 100),
+            SizedBox(height: 50),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(color: Colors.grey[500]),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(36, 81, 80, 82),
+              ),
+            ),
+            SizedBox(height: 30),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: TextStyle(color: Colors.grey[500]),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: const Color.fromARGB(36, 81, 80, 82),
+                suffixIcon: Icon(
+                  Icons.visibility_outlined,
+                  color: Colors.grey[500],
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Checkbox(
+                  side: BorderSide(color: Colors.grey),
+                  value: rememberMe,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      rememberMe = value!;
+                    });
+                  },
+                ),
+                Text('Remember me'),
+              ],
+            ),
+            SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 14, 140, 243),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const DashboardPage(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Text(
+                'OR',
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 225, 225, 227),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Login with Google',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 90),
+            Center(
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(fontSize: 14, color: Colors.black),
+                  children: <TextSpan>[
+                    TextSpan(text: "Don't have an account?"),
+                    TextSpan(
+                      text: ' Register',
+                      style: TextStyle(color: Colors.blue),
+                      recognizer:
+                          TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => const RegisterPage(),
+                                ),
+                              );
+                            },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(80.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Hello Sovannarith, Welcome!',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
+                  },
+                  child: Text('Log Out', style: TextStyle(color: Colors.white)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
